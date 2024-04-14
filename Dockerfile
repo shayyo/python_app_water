a6554c4315f7cb7d51f7a26a5c0bde8c2d95f06a
shay.yosef@gmail.com <shay.yosef@gmail.com>
2024-04-14 18:02:22 +0300

    edit Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,4 @@ RUN apt update
 RUN apt install redis-server dnsutils curl -y
 RUN mkdir /currency
 
-ADD 2020.txt /currency
-
 ADD entrypoint.sh /usr/local/bin/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ IFS=$'\t\n'
 
 redis-server /redis-master/redis.conf
 
-curl -s 
+curl -s https://raw.githubusercontent.com/shayyo/python_app_water/main/currency_data.txt | redis-cli 
 
