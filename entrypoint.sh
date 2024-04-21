6803adf4ee812c370184cf08320416c48e3c21d7
shay.yosef@gmail.com <shay.yosef@gmail.com>
2024-04-21 15:56:36 +0300

    edited yaml

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 IFS=$'\t\n'
 
-redis-server /redis-master/redis.conf
+redis-server /redis-master/redis.conf 
 
-curl -s https://raw.githubusercontent.com/shayyo/python_app_water/main/currency_data.txt | redis-cli 
+curl -s https://raw.githubusercontent.com/shayyo/python_app_water/main/currency_data.txt > /currency_data.txt && cat /currency_data.txt | redis-cli || echo "Error writing datafile to redis" 
 
+tail -f /dev/null
