200eeb35e267892be2991a34ccf8cd342f75e197
shay <shay.yosef@gmail.com>
2024-04-22 13:54:05 +0300

    changes

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -4,6 +4,6 @@ IFS=$'\t\n'
 
 redis-server /redis-master/redis.conf 
 
-curl -s https://raw.githubusercontent.com/shayyo/python_app_water/main/currency_data.txt > /currency_data.txt && cat /currency_data.txt | redis-cli || echo "Error writing datafile to redis" 
+curl -s https://raw.githubusercontent.com/shayyo/python_app_water/main/database/currency_data.txt > /currency_data.txt && cat /currency_data.txt | redis-cli || echo "Error writing datafile to redis" 
 
 tail -f /dev/null
--- flask/app.py
+++ flask/app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def hello_geek():
     return '<h1>Hello from Flask & Docker</h2>'
 
+@app.route('/shay')
+def hello_shay():
+    return '<h1>Hello Shay</h2>'
 
 if __name__ == "__main__":
     app.run(debug=True)
