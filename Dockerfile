de9cad0aac07fc13c36c538f45a92c945da958be
shay <shay.yosef@gmail.com>
2024-04-22 11:28:46 +0300

    files change

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM ubuntu:18.04
-
-RUN apt update
-RUN apt install redis-server dnsutils curl -y
-RUN mkdir /currency
-
-ADD entrypoint.sh /usr/local/bin/entrypoint.sh
--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-IFS=$'\t\n'
-
-redis-server /redis-master/redis.conf 
-
-curl -s https://raw.githubusercontent.com/shayyo/python_app_water/main/currency_data.txt > /currency_data.txt && cat /currency_data.txt | redis-cli || echo "Error writing datafile to redis" 
-
-tail -f /dev/null
