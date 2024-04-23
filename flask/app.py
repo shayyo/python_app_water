7ef71585b14a808ed0dcbd6b48ccc328902a43f2
shay <shay.yosef@gmail.com>
2024-04-23 18:32:43 +0300

    revision in app.py

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,22 @@
+import time
+
+import redis
 from flask import Flask
+
 app = Flask(__name__)
+res = redis.StrictRedis(host='redis-service', port=6379)
+
+def get_currency_data():
+    retries = 5
+    while True:
+        try:
+            return res.get('1990-01-02_AUD')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
 
 @app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
-
-@app.route('/shay')
-def hello_shay():
-    return '<h1>Hello Shay</h2>'
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+def hello():
+    count = get_currency_data()
+    return 'Your currency data is {} times.\n'.format(count)
