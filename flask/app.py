e19355edc6b277c0bf1c557baee24365c641e0df
shay.yosef@gmail.com <shay.yosef@gmail.com>
2024-04-24 13:38:43 +0300

    change code in app.py.currency

--- flask/app.py
+++ flask/app.py
@@ -1,22 +1,11 @@
-import time
-
-import redis
 from flask import Flask
+from redis import Redis
 
 app = Flask(__name__)
-res = redis.StrictRedis(host='redis-service', port=6379)
+redis = Redis(host='redis-service', port=6379)
 
-def get_currency_data():
-    retries = 5
-    while True:
-        try:
-            return res.get('1990-01-02_AUD')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
 
 @app.route('/')
-def hello():
-    count = get_currency_data()
-    return 'Your currency data is {} times.\n'.format(count)
+def index():
+    redis.incr('hits')
+    return 'This page has been visited {} times.'.format(redis.get('hits'))
