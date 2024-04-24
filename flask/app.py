26eb77d3a642434a8adf703d22a93aadaa931eab
shay.yosef@gmail.com <shay.yosef@gmail.com>
2024-04-24 11:55:10 +0300

    change

--- flask/app.py
+++ flask/app.py
@@ -6,17 +6,22 @@ from flask import Flask
 app = Flask(__name__)
 res = redis.StrictRedis(host='redis-service', port=6379)
 
-def get_currency_data():
+def get_currency_data(date_string):
     retries = 5
     while True:
         try:
-            return res.get('1990-01-02_AUD')
+            #return res.get('1990-01-02_AUD')
+            return res.get(date_string)
         except redis.exceptions.ConnectionError as exc:
             if retries == 0:
                 raise exc
             retries -= 1
 
-@app.route('/')
+#@app.route('/')
+#def hello():
+#    count = get_currency_data()
+#    return 'Your currency data is {} times.\n'.format(count)
+@app.route('/<regex("[0-9]{4}-[0-9]{2}-[0-9]{2}"):date_string>')
 def hello():
-    count = get_currency_data()
-    return 'Your currency data is {} times.\n'.format(count)
+    currency_results = get_currency_data(date_string)
+    return f'Your currency data is {}.\n'.(currency_results)
